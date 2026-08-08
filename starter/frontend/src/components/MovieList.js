19ae67d0a645a5c99d6a112dbc0a1e16aafa0031
tanyatarun18 <tanyatarun03@gmail.com>
2026-08-08 14:03:24 +0530

    Add CI CD pipelines

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -1,20 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
 
 function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
-      setMovies(response.data.movies);
-    });
+    axios
+      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
+      .then((response) => {
+        console.log("API Response:", response.data);
+
+        if (Array.isArray(response.data)) {
+          setMovies(response.data);
+        } else if (Array.isArray(response.data.movies)) {
+          setMovies(response.data.movies);
+        } else {
+          console.error("Unexpected API response:", response.data);
+          setMovies([]);
+        }
+      })
+      .catch((error) => {
+        console.error("API Error:", error);
+        setMovies([]);
+      });
   }, []);
 
   return (
     <ul>
       {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+        <li
+          className="movieItem"
+          key={movie.id}
+          onClick={() => onMovieClick(movie)}
+        >
           {movie.title}
         </li>
       ))}
